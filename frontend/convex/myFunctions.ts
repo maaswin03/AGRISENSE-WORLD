56ba50a7424c6f5c0fd0331de9b548e3c47383c7
maaswin03 <m.a.aswin2005@gmail.com>
2024-09-15 21:05:40 +0530

    new

--- frontend/convex/myFunctions.ts
+++ frontend/convex/myFunctions.ts
@@ -1,6 +1,6 @@
-import { v } from "convex/values";
-import { query, mutation, action } from "./_generated/server";
-import { api } from "./_generated/api";
+// import { v } from "convex/values";
+import { query} from "./_generated/server";
+// import { api } from "./_generated/api";
 
 // // Write your Convex functions in any file inside this directory (`convex`).
 // // See https://docs.convex.dev/functions for more.
